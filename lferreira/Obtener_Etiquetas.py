ca1154037862d5d5dfb5f076b37ff40748c0a166
lferreira <lferreira2017@alu.uct.cl>
2019-09-11 18:03:06 -0300

    Mejora codigo obtener_etiquetas.py

--- lferreira/Obtener_Etiquetas.py
+++ lferreira/Obtener_Etiquetas.py
@@ -1,25 +1,51 @@
 import pymongo 
 import nltk
 import nltk.data
+import time as t
+import sys
+import json
 from pymongo import MongoClient
+from tqdm import tqdm
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
-
+#----------------------------------
+#Conexion con mongodb 
+#-----------------------------------
 con = MongoClient("Localhost",27017)
 db  = con.parlamento
 
-#Coleccion 
+#----------------------------------
+#Carga Coleccion 
+#----------------------------------
 quevotan = db.quevotan
-
 data = quevotan.find_one()
-
 sesiones = data['sesiones']
-for i in range(len(data['sesiones'])):
+
+Dic_palabras = {}
+dic_boletin  = {}
+for i in tqdm(range(len(data['sesiones']))):
 	boletin = sesiones[str(i)]['Boletin']
 	for y in range(len(boletin)):
+		#----------------------------------
+		#Procesamiento de texto con NLTK --
+		#----------------------------------
 		detalle = boletin[str(y)]['detalle']
-		print detalle
+		palabras = nltk.word_tokenize(detalle)
+		texto    = nltk.Text(palabras)
+		tags     = nltk.pos_tag(palabras)
+		a_tags =[]
+		for i2 in tags :
+			if i2[1] == 'FW':
+				a_tags.append(i2[0])
+
+		dic_boletin[y]={'id_proyecto':boletin[str(y)]['id'],'palabras':a_tags}
 
 
+		#print boletin
+	Dic_palabras[i] = {"id_sesion":sesiones[str(i)]['Id_sesion'],"boletin":dic_boletin}
 
 
+with open('Palabras.json', 'w') as file:
+    json.dump(Dic_palabras, file)
